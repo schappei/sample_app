eb1e1ccd770d81c4ab204d209864a3c85fe27086
David Fong <david@david-K55A.(none)>
2012-09-18 23:07:07 -0500

    user controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+  get "users/new"
+  match '/home',  :to =>'pages#home'
+  match '/signup',  :to => 'users#new'
 
-  # The priority is based upon order of creation:
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
+  #The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
