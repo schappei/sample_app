db2ae1962243032f72c875c73d2e1118b99a8cfa
David Fong <david@david-K55A.(none)>
2012-09-18 23:32:43 -0500

    User model v1

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe User do
+  before(:each) do
+    @attr = { :name => "Example User", :email => "user@example.com" }
+  end
+  .
+  .
+  .
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+end
