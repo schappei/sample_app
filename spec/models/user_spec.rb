5960dd00a9c1ff71de94ededd0ddedd9e996609e
David Fong <david@david-K55A.(none)>
2012-10-02 14:33:46 -0500

    Go Go GO!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,6 @@ describe User do
   before(:each) do
     @attr = { :name => "Example User", :email => "user@example.com" }
   end
-  .
-  .
-  .
   it "should reject names that are too long" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name))
